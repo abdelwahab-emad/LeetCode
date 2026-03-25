e30d25de96a1b658807bef50847e928b99355c13
abdelwahab <abd916587@gmail.com>
2026-03-25 22:17:35 +0200

    Time: 0 ms (100%), Space: 32 MB (42.28%) - LeetHub

---
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        auto solve = [&](vector<int>& a) {
+            sort(a.begin(), a.end());
+            int best = 1, cur = 1;
+            for (int i = 1; i < a.size(); i++) {
+                if (a[i] == a[i - 1] + 1) cur++;
+                else cur = 1;
+                best = max(best, cur);
+            }
+            return best + 1;
+        };
+
+        int h = solve(hBars);
+        int v = solve(vBars);
+        int side = min(h, v);
+        return side * side;
+    }
+};
