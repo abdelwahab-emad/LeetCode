1f3bd71acbfdfa3975fe624108a11a6f2ae9dfb1
abdelwahab <abd916587@gmail.com>
2026-04-10 20:03:35 +0200

    Time: 23 ms (7.56%), Space: 29.8 MB (31.72%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        int ans = 1e8;
-        for(int i = 0;i < nums.size();i++){
-            for(int j = i + 1;j < nums.size();j++){
-                for(int k = j + 1;k < nums.size();k++){
-                    if(nums[i] == nums[j] && nums[i] == nums[k]){
-                       int dis = abs(i - j) + abs(i - k) + abs(j - k);
-                       ans = min(ans, dis);
-                    }
+        int ans = 1e8, n = nums.size();
+        vector<int> nxt(n + 2, n + 2), lst(n + 2, n + 2);
+        lst[nums[n - 1]] = n - 1;
+        for (int i = n - 2; i >= 0; i--) {
+            nxt[i] = lst[nums[i]];
+            lst[nums[i]] = i;
+        }
+        for (int i = 0; i < n; i++)
+            cout << nxt[i] << " ";
+        for (int i = 0; i < n - 2; i++) {
+            int j = nxt[i], k = -1;
+            if (j < n) {
+                k = nxt[j];
+                if (k < n) {
+                    int dis = abs(i - j) + abs(i - k) + abs(j - k);
+                    ans = min(ans, dis);
                 }
             }
         }
