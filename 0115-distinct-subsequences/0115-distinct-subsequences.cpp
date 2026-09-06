e2f04c47e9c46396aec48f10e7a78f6126a84adb
abdelwahab <abd916587@gmail.com>
2026-09-06 17:55:24 +0300

    Time: 27 ms (71.17%), Space: 27 MB (64.23%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<vector<vector<int>>> dp;
+    vector<vector<int>> dp;
     int n, m;
     string S, T;
-    int go (int i, int j, int take) {
+    int go (int i, int j) {
         if (j == m) return 1;
         if (i == n) {
             return 0;
         }
-        int &ret = dp[i][j][take];
+        int &ret = dp[i][j];
         if (~ret) return ret;
         ret = 0;
-        if (S[i] == T[j]) ret += go(i + 1, j + 1, 1);
-        ret += go(i + 1, j, take);
+        if (S[i] == T[j]) ret += go(i + 1, j + 1);
+        ret += go(i + 1, j);
         return ret;
     }
     int numDistinct(string s, string t) {
         n = s.size();
         m = t.size();
         S = s, T = t;
-        dp = vector<vector<vector<int>>>(n + 1, vector<vector<int>>(m + 1, vector<int>(3, -1)));
-        return go(0, 0, 0);
+        dp = vector<vector<int>>(n + 1, vector<int>(m + 1,  -1));
+        return go(0, 0);
     }
 };
